a4e576cbfb88c7c19ceff79f2d3fd23de39697fc
Cale Sumner <calecsumner@gmail.com>
2025-07-23 10:53:58 -0600

    Add project files.

---
+++ PropertyApp/Control/CustomBorder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls.Shapes;
+
+namespace PropertyApp.Control
+{
+    public class CustomBorder : Border
+    {
+        public CustomBorder()
+        {
+            TranslationX = new Random().Next(-500, 500);
+            Rotation = Math.Max(TranslationX, 100);
+
+            AddCornerRadius();
+
+            Loaded += (s, e) =>
+            {
+                this.TranslateTo(0, 0, 1500, Easing.SinInOut);
+                this.RotateTo(0, 1500, Easing.SinInOut);
+            };
+        }
+
+        private List<int> CornerValues = new List<int> { 10, 40, 70, 100 };
+
+        private void AddCornerRadius()
+        {
+            var index = new Random().Next(4);
+            StrokeShape = new RoundRectangle
+            {
+                CornerRadius = new CornerRadius(CornerValues[index])
+            };
+        }
+    }
+}
